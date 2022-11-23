0207b63b5fc5c55f6c58915eef3ccd1c1a6f9e72
Gaston Herrera Miranda <gastonherreramiranda@gmail.com>
2022-11-23 02:37:51 -0300

    ejecutando lo pedido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import HeadNav from "./components/Navbar";
+import ItemListContainer from "./components/ItemListContainer";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <>
+      <HeadNav />
+      <ItemListContainer mensaje="Aca se pone algo a futuro"/>
+    </>
+  )
 }
 
 export default App;
